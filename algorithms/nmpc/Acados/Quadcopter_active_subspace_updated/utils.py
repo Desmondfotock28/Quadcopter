fd50e68d9bda3441ba998f5fe2d6339b1ee1e608
Desmondfotock28 <fomelackdesmondfotock@gmail.com>
2026-06-11 14:08:46 +0200

    add open loop control data file

--- algorithms/nmpc/Acados/Quadcopter_active_subspace_updated/utils.py
+++ algorithms/nmpc/Acados/Quadcopter_active_subspace_updated/utils.py
@@ -3,11 +3,18 @@ import matplotlib.pyplot as plt
 
 
 def reference_trajectory(t, omega=np.pi):
-    xref = np.zeros(12)
+    xref = np.zeros(13)
     xref[0] = np.sin(omega * t / 10)
     xref[1] = np.cos(omega * t / 10) - 1.0
-    xref[2] = np.sin(omega * t / 10) + t
+    xref[2] = np.sin(omega * t / 10) + t + 1.0
+    xref[3] = 1.0 # qw
+
     return xref
+
+
+def split_stage_vector(u_stack, stage, nu):
+    return u_stack[stage * nu:(stage + 1) * nu]
+
 
 
 def generate_block_identity(n_rows, n_cols):
@@ -18,18 +25,6 @@ def generate_block_identity(n_rows, n_cols):
 
 def split_stage_matrix(t1, stage, nu):
     return t1[stage * nu:(stage + 1) * nu, :]
-
-
-def split_stage_vector(u_stack, stage, nu):
-    return u_stack[stage * nu:(stage + 1) * nu]
-
-
-def reconstruct_input_stack(t1, v_active, mu, inactive_stack):
-    return t1 @ v_active + mu * inactive_stack
-
-
-def shift_input_stack(u_stack, terminal_u, nu):
-    return np.concatenate((u_stack[nu:], terminal_u))
 
 
 def plot_xyz_subplots(t, x_pred, output_path=None, show=True):
@@ -63,7 +58,7 @@ def plot_3d_trajectory(t, x_pred, output_path=None, show=True):
     ax = fig.add_subplot(111, projection="3d")
     t = np.array(t)
     ax.plot(x_pred[:, 0], x_pred[:, 1], x_pred[:, 2], label="Predicted Trajectory", linestyle="--")
-    ax.plot(np.sin(np.pi * t / 10), np.cos(np.pi * t / 10) - 1.0, np.sin(np.pi * t / 10) + t, label="Reference Trajectory", linestyle="--")
+    ax.plot(np.sin(np.pi * t / 10), np.cos(np.pi * t / 10) - 1.0, np.sin(np.pi * t / 10) + t +1, label="Reference Trajectory", linestyle="--")
     ax.set_xlabel("X")
     ax.set_ylabel("Y")
     ax.set_zlabel("Z")
